99dd802931d523611e572c4c8a4115c28bb68dcc
Krfld <rodrigofldias@gmail.com>
2021-04-16 23:14:14 +0100

    I2S Setup

--- Project/main/tools.c
+++ Project/main/tools.c
@@ -1,7 +1,42 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "tools.h"
 
 void delay(int millis)
 {
     vTaskDelay(millis / portTICK_RATE_MS);
+}
+
+void tools()
+{
+    i2s_setup();
+}
+
+void i2s_setup()
+{
+    i2s_config_t i2s_output_config = {
+        .mode = I2S_MODE_MASTER | I2S_MODE_TX,
+        .communication_format = I2S_COMM_FORMAT_STAND_MSB,
+        .sample_rate = 44100,
+        .bits_per_sample = 16,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .dma_buf_count = 8,
+        .dma_buf_len = 64,
+        .intr_alloc_flags = 0, // default interrupt priority
+        .tx_desc_auto_clear = true,
+        .use_apll = false};
+
+    i2s_driver_install(I2S_NUM_0, &i2s_output_config, 0, NULL);
+    i2s_pin_config_t i2s0_pin_config = {
+        .ws_io_num = I2S_WS_PIN(0),
+        .bck_io_num = I2S_WS_PIN(0),
+        .data_out_num = I2S_WS_PIN(0),
+        .data_in_num = I2S_PIN_NO_CHANGE};
+    i2s_set_pin(I2S_NUM_0, &i2s0_pin_config);
+
+    i2s_driver_install(I2S_NUM_1, &i2s_output_config, 0, NULL);
+    i2s_pin_config_t i2s1_pin_config = {
+        .ws_io_num = I2S_WS_PIN(1),
+        .bck_io_num = I2S_WS_PIN(1),
+        .data_out_num = I2S_WS_PIN(1),
+        .data_in_num = I2S_PIN_NO_CHANGE};
+    i2s_set_pin(I2S_NUM_1, &i2s1_pin_config);
 }
--- Project/main/tools.h
+++ Project/main/tools.h
@@ -1,3 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+#include "esp_bt.h"
+#include "bt_app_core.h"
+#include "bt_app_av.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "esp_a2dp_api.h"
+#include "esp_avrc_api.h"
+#include "driver/i2s.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_gatt_common_api.h"
+
 #define BT_BLE_COEX_TAG "SoniqOculos"
 #define BT_DEVICE_NAME "SoniqOculos"
 #define BLE_ADV_NAME "[CONFIG] SoniqOculos"
+
+#define I2S_BCK_PIN(i2s_num) (i2s_num ? 26 : 26)
+#define I2S_WS_PIN(i2s_num) (i2s_num ? 25 : 25)
+#define I2S_DATA_PIN(i2s_num) (i2s_num ? 22 : 22)
+
+void delay(int millis);
+
+void i2s_setup();
+
+void tools();

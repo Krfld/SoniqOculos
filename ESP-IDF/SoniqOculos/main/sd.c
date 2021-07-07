c2c5de852f8d90b8c6e4348b64ebafed2aa130f4
Krfld <rodrigofldias@gmail.com>
2021-07-07 13:23:20 +0100

    .

--- ESP-IDF/SoniqOculos/main/sd.c
+++ ESP-IDF/SoniqOculos/main/sd.c
@@ -5,13 +5,34 @@
 
 #define SD_CARD_TAG "SD_CARD"
 
+typedef struct
+{
+    unsigned char riff[4]; // 'RIFF' string
+    unsigned int size;     // size of file in bytes
+    unsigned char wave[4]; // 'WAVE' string
+
+    unsigned char fmt[4];         // 'fmt ' string with trailing null char
+    unsigned int fmt_size;        // length of the format data
+    unsigned int fmt_type;        // format type. 1-PCM, 3- IEEE float, 6 - 8bit A law, 7 - 8bit mu law
+    unsigned int channels;        // no.of channels
+    unsigned int sample_rate;     // sampling rate (blocks per second)
+    unsigned int byte_rate;       // SampleRate * NumChannels * BitsPerSample/8
+    unsigned int block_align;     // NumChannels * BitsPerSample/8
+    unsigned int bits_per_sample; // bits per sample, 8- 8bits, 16- 16 bits etc
+
+    unsigned char data[4];  // 'data' string
+    unsigned int data_size; // NumSamples * NumChannels * BitsPerSample/8 - size of the next chunk that will be read
+} wav_header_t;
+
 static sdmmc_card_t *card;
 static sdmmc_host_t host = SDSPI_HOST_DEFAULT();
 
 static FILE *f = NULL;
-static bool empty = true;
+static wav_header_t wav_header;
 
 static SemaphoreHandle_t sd_semaphore_handle;
+
+static void wav_header_init();
 
 bool sd_card_state()
 {
@@ -58,7 +79,7 @@ void sd_card_init()
     slot_config.gpio_cs = SD_CS_PIN;
     slot_config.host_id = host.slot;
 
-    if (esp_vfs_fat_sdspi_mount(MOUNT_POINT, &host, &slot_config, &mount_config, &card) != ESP_OK)
+    if (esp_vfs_fat_sdspi_mount(MOUNT_POINT, &host, &slot_config, &mount_config, &card) != ESP_OK) // Mount card
     {
         ESP_LOGW(SD_CARD_TAG, "Reinsert card");
         return;
@@ -66,7 +87,7 @@ void sd_card_init()
 
     ESP_LOGI(SD_CARD_TAG, "Card mounted");
 
-    sd_open_file(FILE_NAME, WRITE);
+    sd_open_file(FILE_NAME, WRITE); // Open file
 
     ESP_LOGW(SD_CARD_TAG, "Free heap: %d", esp_get_free_heap_size());
 }
@@ -83,7 +104,7 @@ void sd_card_deinit()
     if (!get_sd_det_state()) // Check if card was removed unexpectedly
     {
         ESP_LOGE(SD_CARD_TAG, "SD fault");
-        empty = true;
+        wav_header.data_size = 0;
     }
 
     sd_close_file();
@@ -115,16 +136,18 @@ void sd_open_file(char *file, char *type)
 
     char filename[64];
     int32_t file_number = nvs_read(FILE_NAME);
-    sprintf(filename, MOUNT_POINT "/%s_%d.txt", file, file_number);
+    sprintf(filename, MOUNT_POINT "/%s_%d.wav", file, file_number);
 
-    f = fopen(filename, type);
+    f = fopen(filename, type); // Open file
     if (f == NULL)
     {
         ESP_LOGE(SD_CARD_TAG, "Failed to open file");
         return;
     }
 
-    empty = true;
+    wav_header_init();                               // Reset wav header
+    fwrite(&wav_header, sizeof(wav_header_t), 1, f); // Write wav header
+
     ESP_LOGI(SD_CARD_TAG, "File opened");
 }
 
@@ -133,8 +156,14 @@ void sd_close_file()
     if (f == NULL)
         return;
 
-    if (!empty)
-        nvs_write(FILE_NAME, nvs_read(FILE_NAME) + 1);
+    if (wav_header.data_size > 0)
+    {
+        nvs_write(FILE_NAME, nvs_read(FILE_NAME) + 1); // Increment file ID if not empty
+
+        // Rewrite wav header
+        fseek(f, 0, SEEK_SET);
+        fwrite(&wav_header, sizeof(wav_header_t), 1, f);
+    }
 
     fclose(f);
     f = NULL;
@@ -146,12 +175,11 @@ void sd_write_data(uint8_t *data, size_t *len)
     if (f == NULL || !get_sd_det_state())
         return;
 
-    if (empty)
-        empty = false;
-
     xSemaphoreTake(sd_semaphore_handle, portMAX_DELAY);
 
-    fwrite(data, sizeof(*data), *len, f);
+    fwrite(data, sizeof(*data), *len, f); // Write samples
+    wav_header.size += *len;              // Increment size
+    wav_header.data_size += *len;         // Increment data size
 
     xSemaphoreGive(sd_semaphore_handle);
 }
@@ -164,4 +192,35 @@ void sd_card_toggle(bool state)
         sd_card_deinit(); // Close file and unmount SD card
 
     spp_send_msg("s %d", sd_card_state());
+}
+
+static void wav_header_init()
+{
+    wav_header.riff[0] = 'R';
+    wav_header.riff[1] = 'I';
+    wav_header.riff[2] = 'F';
+    wav_header.riff[3] = 'F';
+    wav_header.size = sizeof(wav_header_t) - 8;
+    wav_header.wave[0] = 'W';
+    wav_header.wave[1] = 'A';
+    wav_header.wave[2] = 'V';
+    wav_header.wave[3] = 'E';
+
+    wav_header.fmt[0] = 'f';
+    wav_header.fmt[1] = 'm';
+    wav_header.fmt[2] = 't';
+    wav_header.fmt[3] = ' ';
+    wav_header.fmt_size = FMT_SIZE;
+    wav_header.fmt_type = AUDIO_FORMAT;
+    wav_header.channels = NUM_CHANNELS;
+    wav_header.sample_rate = SAMPLE_RATE;
+    wav_header.byte_rate = BYTE_RATE;
+    wav_header.block_align = BLOCK_ALIGN;
+    wav_header.bits_per_sample = BITS_PER_SAMPLE;
+
+    wav_header.data[0] = 'd';
+    wav_header.data[1] = 'a';
+    wav_header.data[2] = 't';
+    wav_header.data[3] = 'a';
+    wav_header.data_size = 0;
 }
--- ESP-IDF/SoniqOculos/main/sd.h
+++ ESP-IDF/SoniqOculos/main/sd.h
@@ -21,78 +21,15 @@
 
 //* WAV File
 
-/*//Standard values for CD-quality audio
-#define SUBCHUNK1SIZE (16)
-#define AUDIO_FORMAT (1) //For PCM
-#define NUM_CHANNELS (2)
-#define SAMPLE_RATE (44100)
-
-#define BITS_PER_SAMPLE (16)
+#define FMT_SIZE 16
+#define AUDIO_FORMAT 1 // PCM
+#define NUM_CHANNELS 2
+#define SAMPLE_RATE 44100
 
 #define BYTE_RATE (SAMPLE_RATE * NUM_CHANNELS * BITS_PER_SAMPLE / 8)
 #define BLOCK_ALIGN (NUM_CHANNELS * BITS_PER_SAMPLE / 8)
 
-typedef struct wavfile_header_s
-{
-    char ChunkID[4];   //  4
-    int32_t ChunkSize; //  4
-    char Format[4];    //  4
-
-    char Subchunk1ID[4];   //  4
-    int32_t Subchunk1Size; //  4
-    int16_t AudioFormat;   //  2
-    int16_t NumChannels;   //  2
-    int32_t SampleRate;    //  4
-    int32_t ByteRate;      //  4
-    int16_t BlockAlign;    //  2
-    int16_t BitsPerSample; //  2
-
-    char Subchunk2ID[4];
-    int32_t Subchunk2Size;
-} wavfile_header_t;*/
-
-/*typedef struct _wav_header
-{
-    // RIFF Header
-    char riff_header[4]; // Contains "RIFF"
-    int wav_size = 0;    // Size of the wav portion of the file, which follows the first 8 bytes. File size - 8
-    char wave_header[4]; // Contains "WAVE"
-
-    // Format Header
-    char fmt_header[4];      // Contains "fmt " (includes trailing space)
-    int fmt_chunk_size = 16; // Should be 16 for PCM
-    short audio_format = 1;  // Should be 1 for PCM. 3 for IEEE Float
-    short num_channels = 1;
-    int sample_rate = 16000;
-    int byte_rate = 32000;      // Number of bytes per second. sample_rate * num_channels * Bytes Per Sample
-    short sample_alignment = 2; // num_channels * Bytes Per Sample
-    short bit_depth = 16;       // Number of bits per sample
-
-    // Data
-    char data_header[4]; // Contains "data"
-    int data_bytes = 0;  // Number of bytes in data. Number of samples * num_channels * sample byte size
-    // uint8_t bytes[]; // Remainder of wave file is bytes
-
-    _wav_header()
-    {
-        riff_header[0] = 'R';
-        riff_header[1] = 'I';
-        riff_header[2] = 'F';
-        riff_header[3] = 'F';
-        wave_header[0] = 'W';
-        wave_header[1] = 'A';
-        wave_header[2] = 'V';
-        wave_header[3] = 'E';
-        fmt_header[0] = 'f';
-        fmt_header[1] = 'm';
-        fmt_header[2] = 't';
-        fmt_header[3] = ' ';
-        data_header[0] = 'd';
-        data_header[1] = 'a';
-        data_header[2] = 't';
-        data_header[3] = 'a';
-    }
-} wav_header_t;*/
+#define BITS_PER_SAMPLE 16
 
 /**
  * @brief See if card is mounted and file opened
